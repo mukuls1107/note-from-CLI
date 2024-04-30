96636aa2bd1c57d7ee6eb2651cea4662fb0896c8
mukul1107 <mukulsingh3078@gmail.com>
2024-04-30 13:17:59 +0530

    basic structure and setup done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
+import figlet from "figlet";
+import chalk from "chalk";
+// let argument = process.argv[3]
 
-let userNote = process.argv[2];
 
-const userObj = {
-    content: userNote,
-    id: Date.now()
+(function (){
+    draw();
+    beginning()
+})()
+
+
+function beginning(){
+    console.log("Enter your choice: ")
+    let choice = process.argv[2];
+    if(choice == "add" || "a"){
+        console.log(`Adding: ${process.argv[3]}`)
+    }else{
+        console.log("Unknown Choice")
+    }
+    return 0;
 }
 
-console.log(userObj);
+function addToObj(){
+
+}
+
+function deleteFromObj(){
+
+}
+
+
+function display(){
+
+}
+
+function draw()
+{
+    figlet("Notes", function (err, data) {
+        if (err) {
+          console.log("Something went wrong...");
+          console.dir(err);
+          return;
+        }
+        console.log(chalk.blue(data));
+      });
+}
